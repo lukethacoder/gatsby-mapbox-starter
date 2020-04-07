5b2de42833402f26673050472c3d9117a3890b3b
lukethacoder <luke.secomb@live.com>
2020-04-07 23:02:30 +1000

    🔧 fix gatsby build errors

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,10 +3,32 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require("path")
 
 // You can delete this file if you're not using it
-exports.onCreateWebpackConfig = ({ actions }) => {
-  actions.setWebpackConfig({
+exports.onCreateWebpackConfig = ({ actions, stage, loaders }) => {
+  // actions.setWebpackConfig({
+  //   node: { fs: "empty" },
+  // })
+
+  const config = {
     node: { fs: "empty" },
-  })
+    resolve: {
+      modules: [path.resolve(__dirname, "src"), "node_modules"],
+    },
+  }
+
+  // when building HTML, window is not defined, so Leaflet causes the build to blow up
+  if (stage === "build-html") {
+    config.module = {
+      rules: [
+        {
+          test: /mapbox-gl/,
+          use: loaders.null(),
+        },
+      ],
+    }
+  }
+
+  actions.setWebpackConfig(config)
 }
