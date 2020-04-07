af8007923f8627e01c0316e337940c7578f28d58
lukethacoder <luke.secomb@live.com>
2020-04-07 22:42:54 +1000

    💼 add dotenv / fix 'fs' bug / update package config + metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,13 @@
+require("dotenv").config({
+  path: `.env`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Mapbox Starter`,
+    description: `super simple mapbox example / starter for GatsbyJS.`,
+    author: `@lukethacoder`,
+    mapbox_api_key: process.env.GATSBY_MAPBOX_API_KEY,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -22,13 +27,13 @@ module.exports = {
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#222`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
   ],
 }
